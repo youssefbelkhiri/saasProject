be336f520951220e8fe80e16c52e32ab591d309d
hamzaezzine <ezzinehamza070@gmail.com>
2023-11-21 20:08:19 +0100

    User Routes & Add global validation

---
+++ server/src/users/dto/update-user.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, MinLength, IsOptional } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsOptional()
+  password?: string;
+
+  @IsString()
+  @IsOptional()
+  first_name?: string;
+
+  @IsString()
+  @IsOptional()
+  last_name?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Patch, Delete, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Prisma } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @Get()
-  getUser() {
-    return "fd";
+  @Get('/:id')
+  async getUser(@Param('id') id: number) {
+    return await this.userService.getUser(id);
+  }
+
+  @Patch("/:id")
+  async updateUser(@Param('id') id: number,@Body() userData: UpdateUserDto){
+    return await this.userService.updateUser(id,userData);
+  }
+
+  @Delete("/:id")
+  async deleteUser(@Param('id') id: number){
+    return await this.userService.deleteUser(id);
   }
 
 }
