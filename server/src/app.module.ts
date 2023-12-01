8ca8452934f0d20af7f92685c3cd39b81f37ab5b
hamzaezzine <ezzinehamza070@gmail.com>
2023-12-01 17:07:25 +0100

    fix app module problem

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,15 +4,12 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { QuestionsModule } from './questions/questions.module';
 import { ExamModule } from './exam/exam.module';
-import { GroupsService } from './groups/groups.service';
-import { StudentsService } from './students/students.service';
-import { GroupsController } from './groups/groups.controller';
 import { GroupsModule } from './groups/groups.module';
 import { StudentsModule } from './students/students.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, QuestionsModule, ExamModule, EmailModule],
+  imports: [AuthModule, UsersModule, PrismaModule, QuestionsModule, ExamModule, EmailModule, GroupsModule, StudentsModule],
   controllers: [],
   providers: [],
 })
